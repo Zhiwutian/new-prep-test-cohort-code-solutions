98137782dda5d3c703bd25d4a9bb97814545d32a
Brett Albright <balbright@learningfuze.com>
2023-02-21 00:04:56 +0000

    completed build of prep operators and expressions

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,45 @@
+const number1 = 45;
+const number2 = 34;
+const product = number1 * number2;
+
+console.log('product', product);
+console.log('type of product', typeof product);
+
+const charge = 150;
+const payment = 75;
+const amountRemaining = charge - payment;
+
+console.log('amountRemaining', amountRemaining);
+console.log('type ofamountRemaining', typeof amountRemaining);
+
+const tests = 89;
+const assignments = 91;
+const final = 88;
+const grade = (tests + assignments + final) / 3;
+
+console.log('grade', grade);
+console.log('type of grade', typeof grade);
+
+const firstName = 'Brett';
+const lastName = 'Albright';
+const fullName = firstName + ' ' + lastName;
+
+console.log('fullName', fullName);
+console.log('Type of fullname', fullName);
+
+const ph = 5;
+const isAcidic = ph < 7;
+
+console.log('isAcidic', isAcidic);
+console.log('type of isAcidic', typeof isAcidic);
+
+const numberOfSoldiers = 300;
+const isSparta = numberOfSoldiers === 300;
+
+console.log('isSparta', isSparta);
+console.log('typeOf isSparta', typeof isSparta);
+
+let nickName = fullName;
+nickName += ' is the GOAT';
+console.log('nickName', nickName);
+console.log('typeof nickName', typeof nickName);
